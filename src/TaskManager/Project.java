9dae9cf829d18dcee7fd992ee4bd35c8a305960c
Bruno Lannoo <brunolannoo@gmail.com>
2015-02-23 16:21:57 +0100

    Initial classes show project UC

---
+++ src/TaskManager/Project.java
@@ -0,0 +1,18 @@
+package TaskManager;
+
+import java.util.ArrayList;
+
+public class Project {
+public String getStatus() {
+	return null;
+}
+public ArrayList<Task> getAllTasks() {
+	return null;
+}
+public Task getTask(int taskId) {
+	return null;
+}
+public String getDetails() {
+	return null;
+}
+}
---
+++ src/TaskManager/Task.java
@@ -0,0 +1,10 @@
+package TaskManager;
+
+public class Task {
+public String getStatus() {
+	return null;
+}
+public String getDetails() {
+	return null;
+}
+}
