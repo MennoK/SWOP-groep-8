0fbcff0cbd135f155292be0d843d010134c8d600
travolter <jacobs.andre9@gmail.com>
2015-03-09 16:09:27 +0100

    added some throws

--- Project/src/UseCaseTests/UseCase5AdvanceTime.java
+++ Project/src/UseCaseTests/UseCase5AdvanceTime.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import org.junit.Before;
 import org.junit.Test;
 
+import TaskManager.InvalidTimeException;
 import TaskManager.TaskManClock;
 
 public class UseCase5AdvanceTime {
@@ -16,17 +17,17 @@ public class UseCase5AdvanceTime {
 		clock = new TaskManClock(LocalDateTime.of(2015, 03, 01,01,00));
 	}
 	@Test
-	public void testAdvanceTimeFutureSimpleTest() {
+	public void testAdvanceTimeFutureSimpleTest() throws InvalidTimeException {
 		clock.setTime(LocalDateTime.of(2016, 06, 06,06,06));
 		assertEquals(LocalDateTime.of(2016, 06, 06,06,06), clock.getTime());
 	}
 	@Test(expected=IllegalArgumentException.class)
-	public void testAdvanceTimePastSimpleTest() {
+	public void testAdvanceTimePastSimpleTest() throws InvalidTimeException {
 		clock.setTime(LocalDateTime.of(2012, 06, 06,06,06));
 	}
 	//TODO: implement, what changes when time moves on?
 	@Test
-	public void testAdvanceTimeFutureAndCheckChanges() {
+	public void testAdvanceTimeFutureAndCheckChanges() throws InvalidTimeException {
 		clock.setTime(LocalDateTime.of(2016, 06, 06,06,06));
 		assertEquals(LocalDateTime.of(2016, 06, 06,06,06), clock.getTime());
 	}
