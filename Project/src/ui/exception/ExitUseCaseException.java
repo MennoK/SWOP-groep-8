8f6506a79501c8fb63cbcb27e8cac707cc5f6fbe
Bruno Lannoo <brunolannoo@gmail.com>
2015-03-12 17:31:22 +0100

    added serial id to exitToMainMenu exception

--- Project/src/ui/exception/ExitUseCaseException.java
+++ Project/src/ui/exception/ExitUseCaseException.java
@@ -1,6 +1,9 @@
 package ui.exception;
 
 public class ExitUseCaseException extends Exception {
+
+	private static final long serialVersionUID = -5302748863619684144L;
+
 	public ExitUseCaseException(String message) {
 		super(message);
 	}
