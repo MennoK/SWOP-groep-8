ba89333aa4e4f3dd181ad97d8b7578e1e63b7847
Thomas Marcelis <thomasmarcelis@gmail.com>
2015-03-13 12:04:05 +0100

    Fix getCurrentDelay :dag2:

--- Project/src/taskManager/Project.java
+++ Project/src/taskManager/Project.java
@@ -316,16 +316,17 @@ public class Project {
 		if (finishedOnTime() == ProjectFinishingStatus.ON_TIME)
 			throw new IllegalStateException(
 					"Can not ask the current delay of a task which is expected to finish on time");
+		if(this.getAllTasks().size() == 0)
+			throw new IllegalStateException("A project without tasks can't have a delay");
 
-		Duration currentDelay = Duration.ofHours(0);
-
+		Task latestFinishingTask = this.getAllTasks().get(0);
+		
 		for (Task task : getAllTasks()) {
-			if (task.getEstimatedFinishTime().isAfter(getDueTime())) {
-				currentDelay = WorkTime.durationBetween(getDueTime(),
-						task.getEstimatedFinishTime());
+			if(task.getEstimatedFinishTime().isAfter(latestFinishingTask.getEstimatedFinishTime())) {
+				latestFinishingTask = task;
 			}
 		}
-		return currentDelay;
+		return WorkTime.durationBetween(dueTime, latestFinishingTask.getEstimatedFinishTime());
 	}
 
 	/**
