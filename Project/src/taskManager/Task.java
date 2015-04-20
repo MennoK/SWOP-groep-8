7eff91d6b7ada7202ce953759b2eb5883fd61968
Thomas Marcelis <thomasmarcelis@gmail.com>
2015-04-20 15:31:02 +0200

    Add taskmemento

--- Project/src/taskManager/Task.java
+++ Project/src/taskManager/Task.java
@@ -47,7 +47,9 @@ public class Task implements Summarizable {
 	private LocalDateTime endTime;
 	private LocalDateTime startTime;
 	private LocalDateTime lastUpdateTime;
-	
+
+	private TaskMemento memento;
+
 	private Planning planning;
 
 	private static AtomicInteger idCounter = new AtomicInteger(1);
@@ -314,8 +316,17 @@ public class Task implements Summarizable {
 	}
 
 	/**
-	 * This method returns true if and only if the given dependency is not yet
-	 * in the dependency list
+	 * String str = toSummary() + ": "; str += getDescription() + ", "; str +=
+	 * getEstimatedDuration().toHours() + " hours, "; str +=
+	 * getAcceptableDeviation() * 100 + "% margin"; if
+	 * (!getDependencies().isEmpty()) { str += ", depends on {"; for (Task dep :
+	 * getDependencies()) str += " task " + dep.getId(); str += " }"; } if
+	 * (getOriginal() != null) str += ", alternative for task " +
+	 * getOriginal().getId(); if (this.getStatus() == TaskStatus.FINISHED) { str
+	 * += ", started " + getStartTime(); str += ", finished " + getEndTime();
+	 * str += " (" + getFinishStatus() + ")"; } return str; This method returns
+	 * true if and only if the given dependency is not yet in the dependency
+	 * list
 	 * 
 	 * @param dependency
 	 * @return true if and only if the given task dependency is valid
@@ -452,7 +463,17 @@ public class Task implements Summarizable {
 	 * Allows the user to update the status of a Task to finished or failed
 	 * 
 	 * @param startTime
-	 *            : the time at which the user started working on the task.
+	 *            : String str = toSummary() + ": "; str += getDescription() +
+	 *            ", "; str += getEstimatedDuration().toHours() + " hours, ";
+	 *            str += getAcceptableDeviation() * 100 + "% margin"; if
+	 *            (!getDependencies().isEmpty()) { str += ", depends on {"; for
+	 *            (Task dep : getDependencies()) str += " task " + dep.getId();
+	 *            str += " }"; } if (getOriginal() != null) str +=
+	 *            ", alternative for task " + getOriginal().getId(); if
+	 *            (this.getStatus() == TaskStatus.FINISHED) { str +=
+	 *            ", started " + getStartTime(); str += ", finished " +
+	 *            getEndTime(); str += " (" + getFinishStatus() + ")"; } return
+	 *            str; the time at which the user started working on the task.
 	 * @param endTime
 	 *            : the time at which the user stopped working on the task.
 	 * @param setToFail
@@ -665,28 +686,32 @@ public class Task implements Summarizable {
 	public Duration getDuration() {
 		return this.estimatedDuration;
 	}
+
 	/**
 	 * returns the planning of the task, if it has one
+	 * 
 	 * @return
 	 */
 	public Planning getPlanning() {
-		if(this.hasPlanning()){
+		if (this.hasPlanning()) {
 			return planning;
-		}
-		else throw new NullPointerException("the task has no planning");
+		} else
+			throw new NullPointerException("the task has no planning");
 	}
 
 	/**
 	 * sets a planning for the task
+	 * 
 	 * @param planning
 	 */
 	public void setPlanning(Planning planning) {
 		this.planning = planning;
 	}
-	public boolean hasPlanning(){
+
+	public boolean hasPlanning() {
 		return (planning != null);
 	}
-	
+
 	/**
 	 * Partial toString method
 	 * 
@@ -722,6 +747,79 @@ public class Task implements Summarizable {
 		return str;
 	}
 
-	
-	
+	void save() {
+		this.memento = new TaskMemento(this);
+	}
+
+	boolean load() {
+		if(this.memento == null) {
+			return false;
+		}
+		else {
+			this.memento.load(this);
+			return true;
+		}
+	}
+
+	private class TaskMemento {
+
+		private String description;
+		private Duration estimatedDuration;
+		private double acceptableDeviation;
+
+		private List<Task> dependencies;
+		private Map<ResourceType, Integer> requiredResourceTypes;
+		private Task originalTask;
+		private boolean failed = false;
+
+		private LocalDateTime endTime;
+		private LocalDateTime startTime;
+		private LocalDateTime lastUpdateTime;
+
+		private Planning planning;
+
+		private int id;
+
+		// Het aanmaken van het memento object
+		// Alles moet gekopieerd worden, behalve referenties naar objecten in
+		// ons domein
+		// bv. de dependencies worden shallow gekopieerd
+		public TaskMemento(Task task) {
+			this.description = new String(task.description);
+			this.estimatedDuration = task.estimatedDuration.plusSeconds(0);
+			this.acceptableDeviation = new Double(task.acceptableDeviation);
+
+			this.dependencies = new ArrayList<Task>(task.dependencies);
+			this.requiredResourceTypes = new LinkedHashMap<ResourceType, Integer>(
+					task.requiredResourceTypes);
+			this.originalTask = task.originalTask;
+			this.failed = task.failed;
+
+			this.endTime = task.endTime.plusSeconds(0);
+			this.startTime = task.startTime.plusSeconds(0);
+			this.lastUpdateTime = task.lastUpdateTime.plusSeconds(0);
+
+			this.planning = task.planning;
+			this.id = task.id;
+		}
+
+		public void load(Task task) {
+			task.description = this.description;
+			task.estimatedDuration = this.estimatedDuration;
+			task.acceptableDeviation = this.acceptableDeviation;
+
+			task.dependencies = this.dependencies;
+			task.requiredResourceTypes = this.requiredResourceTypes;
+			task.originalTask = this.originalTask;
+			task.failed = this.failed;
+
+			task.endTime = this.endTime;
+			task.startTime = this.startTime;
+			task.lastUpdateTime = this.lastUpdateTime;
+
+			task.planning = this.planning;
+			task.id = this.id;
+		}
+	}
+
 }
