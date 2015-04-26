75f38d6656db3fce875dab78472a466cba4053fe
Thomas Marcelis <thomasmarcelis@gmail.com>
2015-04-26 15:58:26 +0200

    WorkTime tests

--- Project/test/utility/WorkTimeTester.java
+++ Project/test/utility/WorkTimeTester.java
@@ -12,26 +12,37 @@ public class WorkTimeTester {
 
 	@Before
 	public void setUp() throws Exception {
-		
+
 	}
 
 	@Test
 	public void testEstimatedFinishTime() {
-		LocalDateTime finishtime = WorkTime.getFinishTime(LocalDateTime.of(2015, 04, 22, 15, 0), Duration.ofHours(1));
+		LocalDateTime finishtime = WorkTime.getFinishTime(
+				LocalDateTime.of(2015, 04, 22, 15, 0), Duration.ofHours(1));
 		assertEquals(LocalDateTime.of(2015, 04, 22, 16, 0), finishtime);
 	}
-	
+
 	@Test
 	public void testDurationBetween() {
 		LocalDateTime first = LocalDateTime.of(2015, 04, 22, 9, 0);
 		LocalDateTime second = LocalDateTime.of(2015, 04, 23, 17, 0);
-		assertEquals(Duration.ofHours(15), WorkTime.durationBetween(first, second));
+		assertEquals(Duration.ofHours(15),
+				WorkTime.durationBetween(first, second));
 	}
-	
+
 	@Test
 	public void testEstimatedFinishTimeLong() {
-		LocalDateTime finishtime = WorkTime.getFinishTime(LocalDateTime.of(2015, 04, 22, 15, 0), Duration.ofHours(3));
-		assertEquals(LocalDateTime.of(2015, 04, 23, 9, 0), finishtime);
+		LocalDateTime finishtime = WorkTime.getFinishTime(
+				LocalDateTime.of(2015, 04, 22, 15, 0), Duration.ofHours(19));
+		assertEquals(LocalDateTime.of(2015, 04, 27, 9, 0), finishtime);
+	}
+
+	@Test
+	public void testDurationBetweenLong() {
+		LocalDateTime first = LocalDateTime.of(2015, 04, 22, 9, 0);
+		LocalDateTime second = LocalDateTime.of(2015, 04, 27, 17, 0);
+		assertEquals(Duration.ofHours(31),
+				WorkTime.durationBetween(first, second));
 	}
 
 }
