6a805da869be67c3c15e872f31867284f439e528
Thomas Marcelis <thomasmarcelis@gmail.com>
2015-04-30 11:57:44 +0200

    Why did the package disappear?????????/

--- Project/src/taskManager/TimeObserver.java
+++ Project/src/taskManager/TimeObserver.java
@@ -1,9 +1,9 @@
-																																																											package taskManager;
+package taskManager;
 
 import java.time.LocalDateTime;
 
 public interface TimeObserver {
-	
+
 	void handleTimeChange(LocalDateTime time);
 
 }
