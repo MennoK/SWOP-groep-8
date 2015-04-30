6fcba0f49d9843b6853d83feae45a408275e222c
MennoK <menno.keustermans@student.kuleuven.be>
2015-04-30 14:26:02 +0200

    doc

--- Project/src/taskManager/TimeObserver.java
+++ Project/src/taskManager/TimeObserver.java
@@ -2,8 +2,19 @@ package taskManager;
 
 import java.time.LocalDateTime;
 
+/**
+ * Time observer interface
+ * 
+ * @author groep 8
+ *
+ */
 public interface TimeObserver {
 
+	/**
+	 * handles time changes
+	 * 
+	 * @param time
+	 */
 	void handleTimeChange(LocalDateTime time);
 
 }
