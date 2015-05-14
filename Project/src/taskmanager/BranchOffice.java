7f02481ac9498dd215045c77edad320836d1b599
Thomas Marcelis <thomasmarcelis@gmail.com>
2015-05-14 13:03:01 +0200

    Add memento to DelegatedTaskExpert

--- Project/src/taskmanager/BranchOffice.java
+++ Project/src/taskmanager/BranchOffice.java
@@ -8,8 +8,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import com.sun.istack.internal.NotNull;
-
 import utility.TimeSpan;
 
 /**
--- Project/src/taskmanager/DelegatedTaskExpert.java
+++ Project/src/taskmanager/DelegatedTaskExpert.java
@@ -7,6 +7,7 @@ import java.util.Set;
 public class DelegatedTaskExpert {
 
 	private Set<Task> delegatedTasks;
+    private Memento memento;
 	
 	/**
 	 * Default constructor of the delegatedTaskExpert. It initializes a 
@@ -32,6 +33,58 @@ public class DelegatedTaskExpert {
 	 */
 	public Set<Task> getAllDelegatedTasks(){
 		return Collections.unmodifiableSet(delegatedTasks);
+	}
+	/**
+	 * Saves the current state of the class
+	 */
+	void save() {
+		this.memento = new Memento();
+	}
+
+	/**
+	 * Loads the last save state of the class
+	 * 
+	 * @return last state of the class
+	 */
+	void load() {
+		if (this.memento == null) {
+			throw new IllegalStateException(
+					"You need to save before you can load");
+		} else {
+			this.memento.load();
+		}
+	}
+
+	/**
+	 * 
+	 * Inner momento class of developer expert
+	 * 
+	 * @author groep 8
+	 */
+	private class Memento {
+		private Set<Task> delegatedTasks;
+
+		/**
+		 * Constructor of the momento inner class of developer expert.
+		 * Initialize a new set of developers of the current state
+		 * 
+		 * @param de
+		 *            : developerExpert
+		 */
+		public Memento() {
+            this.delegatedTasks = new LinkedHashSet<Task>(DelegatedTaskExpert.this.delegatedTasks);
+		}
+
+		/**
+		 * Sets the developer set of the developer class to the saved set of the
+		 * momento class
+		 * 
+		 * @param de
+		 *            : developer expert
+		 */
+		public void load() {
+			DelegatedTaskExpert.this.delegatedTasks = this.delegatedTasks;
+		}
 	}
 	
 }
