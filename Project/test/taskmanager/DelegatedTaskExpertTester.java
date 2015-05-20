718757dab29daf176fead8d470a17db54177140d
Bruno Lannoo <brunolannoo@gmail.com>
2015-05-20 15:15:02 +0200

    I just stole your car BUG

--- Project/test/taskmanager/DelegatedTaskExpertTester.java
+++ Project/test/taskmanager/DelegatedTaskExpertTester.java
@@ -9,10 +9,12 @@ import java.time.Duration;
 import org.junit.Before;
 import org.junit.Test;
 
+import taskmanager.exception.UnplannableResourceTypeException;
 
 public class DelegatedTaskExpertTester extends TaskManTester {
-	
-	BranchOffice bruggeOffice;
+
+	private BranchOffice bruggeOffice;
+	private Project project;
 
 	@Before
 	public void setUp() {
@@ -21,29 +23,47 @@ public class DelegatedTaskExpertTester extends TaskManTester {
 		this.tmc.logIn(bruggeOffice);
 		Developer me = this.tmc.createDeveloper("Jozef Stalin");
 		this.tmc.logIn(me);
+		project = this.createStandardProject(time.plusHours(300));
 	}
-	
+
 	@Test
 	public void delegateTaskTest() {
-		Project myProject = this.createStandardProject(time.plusHours(300));
-		Task myTask = this.createTask(myProject, Duration.ofHours(5));
+		Task myTask = this.createTask(project, Duration.ofHours(5));
 		this.tmc.delegate(myTask, here);
-		assertTrue(here.getDelegatedTaskExpert().getAllDelegatedTasks().contains(myTask));
-		assertTrue(here.getDelegatedTaskExpert().getOriginalOffice(myTask).equals(bruggeOffice));
+		assertTrue(here.getDelegatedTaskExpert().getAllDelegatedTasks()
+				.contains(myTask));
+		assertTrue(here.getDelegatedTaskExpert().getOriginalOffice(myTask)
+				.equals(bruggeOffice));
 		this.tmc.logIn(here);
 		this.tmc.logIn(dev);
 		this.tmc.delegate(myTask, bruggeOffice);
-		assertFalse(here.getDelegatedTaskExpert().getAllDelegatedTasks().contains(myTask));
-		assertTrue(bruggeOffice.getDelegatedTaskExpert().getAllDelegatedTasks().contains(myTask));
+		assertFalse(here.getDelegatedTaskExpert().getAllDelegatedTasks()
+				.contains(myTask));
+		assertTrue(bruggeOffice.getDelegatedTaskExpert().getAllDelegatedTasks()
+				.contains(myTask));
 	}
-	
+
+	@Test(expected = UnplannableResourceTypeException.class)
+	public void planDelegatedTaskNoRessourceAvaillable() {
+		ResourceType car = ResourceType.builder("car").build(
+				tmc.getActiveOffice());
+		car.createResource("red car");
+		Task task = Task.builder("desc", Duration.ofHours(8), 0.5)
+				.addRequiredResourceType(car, 1).build(project);
+		tmc.delegate(task, bruggeOffice);
+		tmc.logOut();
+		tmc.logIn(bruggeOffice);
+		Developer dev = tmc.createDeveloper("Office2Worker");
+		tmc.getPlanner().createPlanning(time, task, dev).build();
+	}
+
 	@Test
 	public void mementoTest() {
-		Project myProject = this.createStandardProject(time.plusHours(300));
-		Task myTask = this.createTask(myProject, Duration.ofHours(5));
+		Task myTask = this.createTask(project, Duration.ofHours(5));
 		here.saveSystem(bruggeOffice);
 		this.tmc.delegate(myTask, here);
 		here.loadSystem(bruggeOffice);
-		assertEquals(0, here.getDelegatedTaskExpert().getAllDelegatedTasks().size());
+		assertEquals(0, here.getDelegatedTaskExpert().getAllDelegatedTasks()
+				.size());
 	}
 }
--- Project/test/useCase/UseCase9DelegateTask.java
+++ Project/test/useCase/UseCase9DelegateTask.java
@@ -10,31 +10,31 @@ import org.junit.Before;
 import org.junit.Test;
 
 import taskmanager.BranchOffice;
+import taskmanager.Developer;
 import taskmanager.Project;
+import taskmanager.Resource;
+import taskmanager.ResourceType;
 import taskmanager.Task;
 
 public class UseCase9DelegateTask extends UseCaseTestBasis {
 
 	private Project project;
 	private BranchOffice office2;
-	
+
 	@Before
 	public void setUp() {
 		setUpTMC(LocalDateTime.of(2015, 03, 06, 8, 00));
 		project = tmc.createProject("project", "desc", now.plusDays(4));
 		office2 = tmc.createBranchOffice("Wonderland");
-		
 	}
 
 	@Test
-	public void delegateTask(){
-		Task taskToDelegate = Task.builder("delegate", Duration.ofHours(1), 1).build(project);
+	public void delegateTask() {
+		Task taskToDelegate = Task.builder("delegate", Duration.ofHours(1), 1)
+				.build(project);
 		tmc.delegate(taskToDelegate, office2);
-		
+
 		assertTrue(tmc.getAllDelegatedTasksTo(office2).contains(taskToDelegate));
-		assertEquals(1,tmc.getAllDelegatedTasksTo(office2).size());
-		
-		
-	
+		assertEquals(1, tmc.getAllDelegatedTasksTo(office2).size());
 	}
 }
