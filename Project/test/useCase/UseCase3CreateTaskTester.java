c20caccbfd36a0273d25a26196bf5d228680995f
Bruno Lannoo <brunolannoo@gmail.com>
2015-05-20 12:09:33 +0200

    worked a bit on UC3 create task sequence diagram

--- Project/test/useCase/UseCase3CreateTaskTester.java
+++ Project/test/useCase/UseCase3CreateTaskTester.java
@@ -17,27 +17,28 @@ import taskmanager.Task;
 public class UseCase3CreateTaskTester extends UseCaseTestBasis {
 
 	private ResourceType resType;
+	private Project project;
 
 	@Before
 	public void setUp() {
 		setUpTMC(LocalDateTime.of(2015, 03, 07, 01, 00));
+
+		// set up a project
+		project = tmc.createProject("Project 1", "Description 1", now
+				.minusDays(6).minusHours(1), now.plusDays(3).minusHours(1));
+
+		// set up some available resources
 		resType = ResourceType.builder("resourcetype").build(
 				tmc.getActiveOffice());
-
 		resType.createResource("res1");
 	}
 
 	@Test
 	public void createTask() {
-
-		// create a project
-		Project project1 = tmc.createProject("Project 1", "Description 1", now
-				.minusDays(6).minusHours(1), now.plusDays(3).minusHours(1));
-
 		// create a simple task
 		Task task1 = Task.builder("simple descr", Duration.ofHours(20), 50)
-				.build(project1);
-		assertEquals(1, project1.getAllTasks().size());
+				.build(project);
+		assertEquals(1, project.getAllTasks().size());
 		assertEquals("simple descr", task1.getDescription());
 		assertEquals(Duration.ofHours(20), task1.getEstimatedDuration());
 		assertEquals(50, task1.getAcceptableDeviation(), 0.001);
@@ -45,10 +46,10 @@ public class UseCase3CreateTaskTester extends UseCaseTestBasis {
 		// create a new task dependent on the simple task
 		Task task2 = Task
 				.builder("task with dependency", Duration.ofHours(20), 50)
-				.addDependencies(task1).build(project1);
+				.addDependencies(task1).build(project);
 
-		assertEquals(2, project1.getAllTasks().size());
-		assertTrue(project1.getAllTasks().contains(task2));
+		assertEquals(2, project.getAllTasks().size());
+		assertTrue(project.getAllTasks().contains(task2));
 		assertTrue(task2.getDependencies().contains(task1));
 
 		// create an alternative task for a failed task
@@ -60,18 +61,18 @@ public class UseCase3CreateTaskTester extends UseCaseTestBasis {
 
 		// we create an alternative task for the simple task
 		Task task3 = Task.builder("alternative task", Duration.ofHours(20), 50)
-				.setOriginalTask(task1).build(project1);
+				.setOriginalTask(task1).build(project);
 
-		assertEquals(3, project1.getAllTasks().size());
-		assertTrue(project1.getAllTasks().contains(task3));
+		assertEquals(3, project.getAllTasks().size());
+		assertTrue(project.getAllTasks().contains(task3));
 
 		// task with dependency is now dependent on alternative task
 		assertTrue(task2.getDependencies().contains(task3));
 
 		// task with required resourceType
 		Task task4 = Task.builder("desc", Duration.ofHours(2), 2)
-				.addRequiredResourceType(resType, 1).build(project1);
+				.addRequiredResourceType(resType, 1).build(project);
 		assertEquals(1, task4.getRequiredResourceTypes().size());
-		assertTrue(project1.getAllTasks().contains(task4));
+		assertTrue(project.getAllTasks().contains(task4));
 	}
 }
